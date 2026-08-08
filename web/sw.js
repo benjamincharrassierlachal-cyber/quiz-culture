b37972960c76f291505aecf5624f896cad4148e1
Benjamin Charrassier Lachal <benjamin.charrassier.lachal@gmail.com>
2026-08-08 17:36:07 +0200

    defis : choix de la graine pour limiter les redites

--- web/sw.js
+++ web/sw.js
@@ -1,5 +1,5 @@
 /* Service worker : met l'app en cache pour qu'elle fonctionne hors ligne. */
-var CACHE = "quiz-culture-f56ff8af97";
+var CACHE = "quiz-culture-8cadaddc1e";
 var FILES = ["./", "./index.html", "./manifest.webmanifest",
   "./icons/icon-192.png", "./icons/icon-512.png", "./icons/apple-touch-icon.png", "./icons/favicon-64.png",
   "./img/bac.jpg", "./img/detente.jpg", "./img/defi.jpg",
